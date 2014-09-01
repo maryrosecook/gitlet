6a62955fc993cb8514214ca281f3d55596ea2572
Mary Rose Cook <maryrosecook@maryrosecook.com>
2014-09-01 16:47:37 -0400

    Test update_ref() can update master

--- spec/gimlet.spec.js
+++ spec/gimlet.spec.js
@@ -586,6 +586,23 @@ describe('gimlet', function() {
       expect(fs.readFileSync(".gimlet/HEAD", "utf8")).toEqual("ref: refs/heads/master\n");
       expect(fs.readFileSync(".gimlet/refs/heads/master", "utf8")).toEqual("343b3d02");
     });
+
+    it('should allow update of master to a hash', function() {
+      g.init();
+      createFilesFromTree({ "1": { "filea": "filea", "fileb": "fileb", "2":
+                                   { "filec": "filec", "3a":
+                                     { "filed": "filed", "filee": "filee"}, "3b":
+                                     { "filef": "filef", "fileg": "fileg"}}}});
+
+      g.add("1/2/3a");
+      g.commit({ m: "first", date: new Date(1409404605356) });
+      g.add("1/2/3b");
+      g.commit({ m: "second", date: new Date(1409404605356) });
+
+      expect(fs.readFileSync(".gimlet/refs/heads/master", "utf8")).toEqual("16f3a11f");
+      g.update_ref("refs/heads/master", "343b3d02");
+      expect(fs.readFileSync(".gimlet/refs/heads/master", "utf8")).toEqual("343b3d02");
+    });
   });
 });
 
