53dce260268e52a05a059047de5e66aba634606a
Mary Rose Cook <maryrosecook@maryrosecook.com>
2014-09-08 15:46:44 -0400

    Add tests for running gimlet from the command line

---
+++ spec/gimlet.spec.js
@@ -0,0 +1,91 @@
+var fs = require('fs');
+var g = require('../gimlet');
+var ga = require('../gimlet-api');
+var testUtil = require('./test-util');
+
+describe('gimlet cli', function() {
+  beforeEach(testUtil.createEmptyRepo);
+
+  describe('running each gimlet command under normal circs', function() {
+    it('gimlet init a repo', function() {
+      g(["node", "gimlet.js", "init"]);
+      testUtil.expectFile(__dirname + "/tmp/.gimlet/HEAD", "ref: refs/heads/master\n");
+    });
+
+    it('gimlet add a file', function() {
+      testUtil.createFilesFromTree({ "1": { filea: "filea" }});
+      ga.init();
+      g(["node", "gimlet.js", "add", "1/filea"]);
+      expect(testUtil.index()[0].path).toEqual("1/filea");
+      expect(testUtil.index().length).toEqual(1);
+    });
+
+    it('gimlet make new branch', function() {
+      testUtil.pinDate();
+
+      testUtil.createFilesFromTree({ "1": { filea: "filea" }});
+      ga.init();
+      ga.add("1/filea");
+      ga.commit({ m: "blah" });
+      g(["node", "gimlet.js", "branch", "woo"]);
+      testUtil.expectFile(".gimlet/refs/heads/woo", "6db3fd6a");
+
+      testUtil.unpinDate();
+    });
+
+    it('commit', function() {
+      testUtil.pinDate();
+
+      testUtil.createFilesFromTree({ "1": { filea: "filea" }});
+      ga.init();
+      ga.add("1/filea");
+      g(["node", "gimlet.js", "commit", "-m", "blah"]);
+      testUtil.expectFile(".gimlet/refs/heads/master", "6db3fd6a");
+
+      testUtil.unpinDate();
+    });
+
+    it('hash-object and write', function() {
+      testUtil.createFilesFromTree({ "1": { filea: "filea" }});
+      ga.init();
+      g(["node", "gimlet.js", "hash-object", "1/filea", "-w"]);
+      testUtil.expectFile(".gimlet/objects/5ceba65", "filea");
+    });
+
+    it('create symbolic ref from HEAD to woo branch', function() {
+      ga.init();
+      g(["node", "gimlet.js", "symbolic-ref", "HEAD", "refs/heads/woo"]);
+      testUtil.expectFile(".gimlet/HEAD", "ref: refs/heads/woo\n");
+    });
+
+    it('update HEAD ref to prior commit', function() {
+      testUtil.pinDate();
+
+      ga.init();
+      testUtil.createFilesFromTree({ "1": { filea: "filea", fileb: "fileb", "2":
+                                            { filec: "filec", "3a":
+                                              { filed: "filed", filee: "filee"}, "3b":
+                                              { filef: "filef", fileg: "fileg"}}}});
+
+      ga.add("1/2/3a");
+      ga.commit({ m: "first" });
+      ga.add("1/2/3b");
+      ga.commit({ m: "second" });
+
+      g(["node", "gimlet.js", "update-ref", "HEAD", "343b3d02"]);
+
+      expect(fs.readFileSync(".gimlet/refs/heads/master", "utf8")).toEqual("343b3d02");
+
+      testUtil.unpinDate();
+    });
+
+    it('should be able to write largish tree when no trees written yet', function() {
+      ga.init();
+      testUtil.createFilesFromTree({ "1": { filea: "filea", fileb: "fileb", "2":
+                                            { filec: "filec", "3":
+                                              { filed: "filed", filee: "filee"}}}});
+      ga.add("1");
+      expect(g(["node", "gimlet.js", "write-tree"])).toEqual("7afc965a");
+    });
+  });
+});
