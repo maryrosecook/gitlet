a5b852bea2c18a75fe152c7d72ad29e8470709e1
Mary Rose Cook <maryrosecook@maryrosecook.com>
2014-10-08 19:29:31 -0400

    Test con set branches to track remotes

--- spec/branch.spec.js
+++ spec/branch.spec.js
@@ -56,4 +56,91 @@ describe("branch", function() {
     expect(function() { g.branch("woo") })
       .toThrow("fatal: A branch named 'woo' already exists.");
   });
+
+  it("should throw if -u passed but no commits", function() {
+    g.init();
+    expect(function() { g.branch(undefined, { u: "notthere/whatever" }) })
+      .toThrow("fatal: Not a valid object name: 'master'.");
+  });
+
+  it("should throw if -u passed, there are commits, but remote does not exist", function() {
+    g.init();
+    testUtil.createFilesFromTree({ "1": { "filea": "filea"}});
+    g.add("1/filea");
+    g.commit({ m: "first" });
+
+    expect(function() { g.branch(undefined, { u: "notthere/whatever" }) })
+      .toThrow("error: the requested upstream branch 'notthere/whatever' does not exist");
+  });
+
+  it("should throw if -u and detached head", function() {
+    g.init();
+    testUtil.createStandardFileStructure();
+    g.add("1a/filea");
+    g.commit({ m: "first" });
+    g.checkout("21cb63f6");
+
+    expect(function() { g.branch(undefined, { u: "origin/master" }) })
+      .toThrow("fatal: could not set upstream of HEAD to origin/master" +
+               " when it does not point to any branch.");
+  });
+
+  it("should throw if -u, there are commits, remote exists, rem branch doesn't", function() {
+    g.init();
+    testUtil.createFilesFromTree({ "1": { "filea": "filea"}});
+    g.add("1/filea");
+    g.commit({ m: "first" });
+    g.remote("add", "origin", "file://a/b/c/d");
+
+    expect(function() { g.branch(undefined, { u: "origin/etuteuh" }) })
+      .toThrow("error: the requested upstream branch 'origin/etuteuh' does not exist");
+  });
+
+  it("should set branch to tracking if remote exists", function() {
+    var gl = g, gr = g;
+    var localRepo = process.cwd();
+    var remoteRepo = testUtil.makeRemoteRepo();
+
+    gr.init();
+    testUtil.createStandardFileStructure();
+
+    gr.add("1a/filea");
+    gr.commit({ m: "first" });
+
+    process.chdir(localRepo);
+    gl.init();
+    testUtil.createStandardFileStructure();
+    gr.add("1a/filea");
+    gr.commit({ m: "first" });
+    gl.remote("add", "origin", remoteRepo);
+    gl.fetch("origin");
+
+    g.branch(undefined, { u: "origin/master" })
+    var configFileLines = fs.readFileSync(".gitlet/config", "utf8").split("\n");
+    expect(configFileLines[2]).toEqual("[branch \"master\"]");
+    expect(configFileLines[3]).toEqual("  remote = origin");
+  });
+
+  it("should report branch set to track", function() {
+    var gl = g, gr = g;
+    var localRepo = process.cwd();
+    var remoteRepo = testUtil.makeRemoteRepo();
+
+    gr.init();
+    testUtil.createStandardFileStructure();
+
+    gr.add("1a/filea");
+    gr.commit({ m: "first" });
+
+    process.chdir(localRepo);
+    gl.init();
+    testUtil.createStandardFileStructure();
+    gr.add("1a/filea");
+    gr.commit({ m: "first" });
+    gl.remote("add", "origin", remoteRepo);
+    gl.fetch("origin");
+
+    expect(g.branch(undefined, { u: "origin/master" }))
+      .toEqual("Branch master set up to track remote branch master from origin.");
+  });
 });
