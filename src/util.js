aeaad1611e2d5149825cbd81b9df54ce76c8016a
Mary Rose Cook <maryrosecook@maryrosecook.com>
2014-11-18 16:54:32 -0500

    Add util.unique array fn

--- src/util.js
+++ src/util.js
@@ -38,6 +38,10 @@ var util = module.exports = {
     return a.filter(function(e) { return b.indexOf(e) === -1; });
   },
 
+  unique: function(array) {
+    return array.reduce(function(a, p) { return a.indexOf(p) === -1 ? a.concat(p) : a; }, []);
+  },
+
   intersection: function(a, b) {
     return a.filter(function(e) { return b.indexOf(e) !== -1; });
   },
