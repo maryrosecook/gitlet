7c83a0f0c1d83339f52731dce07713e296e0b937
Mary Rose Cook <maryrosecook@maryrosecook.com>
2014-11-25 14:32:41 -0500

    rm unused fns

--- src/merge.js
+++ src/merge.js
@@ -8,50 +8,6 @@ var refs = require("./refs");
 var util = require("./util");
 
 var merge = module.exports = {
-  longestCommonSubsequence: util.memoize(function(a, b) {
-    if (a.length === 0 || b.length === 0) {
-      return [];
-    } else {
-      var aRemaining = a.slice(0, -1);
-      var bRemaining = b.slice(0, -1);
-
-      if (a[a.length - 1] === b[b.length - 1]) {
-        return merge.longestCommonSubsequence(aRemaining, bRemaining).concat(a[a.length - 1]);
-      } else {
-        var aLcs = merge.longestCommonSubsequence(a, bRemaining);
-        var bLcs = merge.longestCommonSubsequence(aRemaining, b);
-        return aLcs.length > bLcs.length ? aLcs : bLcs;
-      }
-    }
-  }),
-
-  align: function(a, b, lcs, out) {
-    if (lcs === undefined) {
-      lcs = merge.longestCommonSubsequence(a, b);
-      return merge.align(a, b, lcs, { a: [], b: [] });
-    }
-
-    if (a.length === 0 && b.length === 0) {
-      return out;
-    } else if (a[0] === lcs[0] && b[0] === lcs[0]) {
-      out.a.push(lcs[0]);
-      out.b.push(lcs[0]);
-      return merge.align(a.slice(1), b.slice(1), lcs.slice(1), out);
-    } else if (a[0] !== lcs[0] && b[0] !== lcs[0]) {
-      out.a.push(a[0]);
-      out.b.push(b[0]);
-      return merge.align(a.slice(1), b.slice(1), lcs, out);
-    } else if (a[0] === lcs[0]) {
-      out.a.push(undefined);
-      out.b.push(b[0]);
-      return merge.align(a, b.slice(1), lcs, out);
-    } else if (b[0] === lcs[0]) {
-      out.a.push(a[0]);
-      out.b.push(undefined);
-      return merge.align(a.slice(1), b, lcs, out);
-    }
-  },
-
   readCommonAncestor: function(aHash, bHash) {
     var sorted = [aHash, bHash].sort();
     aHash = sorted[0];
