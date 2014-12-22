5a4a2ef53962e18a10c6678095c4568f358b1f3c
Mary Rose Cook <maryrosecook@maryrosecook.com>
2014-12-22 18:07:41 -0500

    Test can fetch to bare repo

--- spec/fetch.spec.js
+++ spec/fetch.spec.js
@@ -49,6 +49,31 @@ describe("fetch", function() {
       });
   });
 
+  it("should be able to fetch objects for main branch on remote for bare repo", function() {
+    var gl = g, gr = g;
+    var localRepo = process.cwd();
+    var remoteRepo = testUtil.makeRemoteRepo();
+
+    gr.init();
+    testUtil.createStandardFileStructure();
+    gr.add("1a/filea");
+    gr.commit({ m: "first" });
+    gr.add("1b/fileb");
+    gr.commit({ m: "second" });
+
+    process.chdir(localRepo);
+    gl.init({ bare: true });
+    gl.remote("add", "origin", remoteRepo);
+    gl.fetch("origin");
+
+    ["17a11ad4", "63e0627e", "17653b6d", "5ceba65", // first commit
+     "16b35712", "794ea686", "507bf191", "5ceba66"] // second commit
+      .forEach(function(h) {
+        var exp = fs.readFileSync(nodePath.join(remoteRepo, ".gitlet", "objects", h), "utf8");
+        testUtil.expectFile(nodePath.join("objects", h), exp);
+      });
+  });
+
   it("should set master to hash value it has on remote", function() {
     var gl = g, gr = g;
     var localRepo = process.cwd();
