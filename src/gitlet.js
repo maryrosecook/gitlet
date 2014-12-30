48affb2a99e5b28c575618e646a6175170c1e669
Mary Rose Cook <maryrosecook@maryrosecook.com>
2014-12-30 10:24:07 +0000

    Pass just branch name to check if remote branch checked out

--- src/gitlet.js
+++ src/gitlet.js
@@ -295,11 +295,8 @@ var gitlet = module.exports = {
       throw "fatal: " + remoteName + " does not appear to be a git repository";
     } else if (config.read().branch[refs.readHeadBranchName()] === undefined) {
       throw "fatal: Current branch " + refs.readHeadBranchName() +" has no upstream branch";
-    } else {
-      var branch = refs.readTerminalRef(refs.readHeadBranchName());
-      if (remote.readCanPushTo(remoteName, branch)) {
-        throw "error: refusing to update checked out branch: " + refs.readHeadBranchName();
-      }
+    } else if (!remote.readIsCheckedOut(remoteName, refs.readHeadBranchName())) {
+      throw "error: refusing to update checked out branch: " + refs.readHeadBranchName();
     }
   },
 
