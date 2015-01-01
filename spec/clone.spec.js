d38cbcc1027d384869b13ff561de095314b99591
Mary Rose Cook <maryrosecook@maryrosecook.com>
2015-01-01 20:08:42 +0000

    clone should throw if target dir not empty

--- spec/clone.spec.js
+++ spec/clone.spec.js
@@ -17,4 +17,16 @@ describe("clone", function() {
     expect(function() { g.clone("a"); })
       .toThrow("you must specify remote path and target path");
   });
+
+  it("should not throw if target path exists and is empty", function() {
+    fs.mkdirSync("exists");
+    g.clone("whatever", "exists");
+  });
+
+  it("should throw if target path exists and is not empty ", function() {
+    fs.mkdirSync("exists");
+    fs.writeFileSync(nodePath.join("exists", "here"), "here");
+    expect(function() { g.clone("whatever", "exists"); })
+      .toThrow("exists already exists and is not empty");
+  });
 });
