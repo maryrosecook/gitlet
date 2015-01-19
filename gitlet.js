8f1730342a82639f75f4663e775d568ee32b7ce4
Mary Rose Cook <maryrosecook@maryrosecook.com>
2015-01-18 21:26:39 -0500

    Add copious comments to diff module

--- gitlet.js
+++ gitlet.js
@@ -1196,22 +1196,61 @@ var index = {
   }
 };
 
+// Diff module
+// -----------
+// Produces diffs between versions of the repository content.  Diffs
+// are represented as JS objects that map file paths to objects that
+// indicate the change required to get from the first version of the
+// file (the receiver) to the second (the giver).  eg:
+// <pre>`{
+//   file1: {
+//     status: "A",
+//     receiver: undefined,
+//     base: undefined,
+//     giver: hash(1)
+//   },
+//   file2: {
+//     status: "C",
+//     receiver: hash(b),
+//     base: hash(a),
+//     giver: hash(c)
+//   }
+// }`</pre>
+
 var diff = {
   FILE_STATUS: { ADD: "A", MODIFY: "M", DELETE: "D", SAME: "SAME", CONFLICT: "CONFLICT" },
 
+  // **diff()** returns a diff object (see above for the format of a
+  // diff object).  If `hash1` is passed, it is used as the first
+  // version in the diff.  If it is not passed, the index is used.  If
+  // `hash2` is passed, it is used as the second version in the diff.
+  // If it is not passed` the working copy is used.
   diff: function(hash1, hash2) {
     var a = hash1 === undefined ? index.toc() : objects.commitToc(hash1);
     var b = hash2 === undefined ? index.workingCopyToc() : objects.commitToc(hash2);
     return diff.tocDiff(a, b);
   },
 
+  // **nameStatus()** takes a diff and returns a JS object that maps
+  // **from file paths to file statuses.
   nameStatus: function(dif) {
     return Object.keys(dif)
       .filter(function(p) { return dif[p].status !== diff.FILE_STATUS.SAME; })
       .reduce(function(ns, p) { return util.assocIn(ns, [p, dif[p].status]); }, {});
   },
 
+  // **tocDiff()** takes three JS objects that map file paths to file
+  // content.  It returns a diff between `receiver` and `giver`
+  // (see the module description for the format).  `base` is the
+  // version that is the most recent commen ancestor of the `receiver`
+  // and `giver`.  If `base` is not passed, `receiver` is used as the
+  // base.  The base is only passed when getting the diff for a merge.
+  // This is the only time the conflict status might be used.
   tocDiff: function(receiver, giver, base) {
+
+    // fileStatus() takes three strings that represent different
+    // versions of the content of a file.  It returns the change that
+    // needs to be made to get from the `receiver` to the `giver`.
     function fileStatus(receiver, giver, base) {
       var receiverPresent = receiver !== undefined;
       var basePresent = base !== undefined;
@@ -1233,8 +1272,13 @@ var diff = {
       }
     };
 
+    // If `base` was not passed, use `receiver` as the base.
     base = base || receiver;
+
+    // Get an array of all the paths in all the versions.
     var paths = Object.keys(receiver).concat(Object.keys(base)).concat(Object.keys(giver));
+
+    // Create and return diff.
     return util.unique(paths).reduce(function(idx, p) {
       return util.assocIn(idx, [p, {
         status: fileStatus(receiver[p], giver[p], base[p]),
@@ -1245,6 +1289,10 @@ var diff = {
     }, {});
   },
 
+  // **changedFilesCommitWouldOverwrite()** gets a list of files
+  // changed in the working copy.  It gets a list of the files that
+  // are different in the head commit and the commit for the passed
+  // hash.  It returns a list of paths that appear in both lists.
   changedFilesCommitWouldOverwrite: function(hash) {
     var headHash = refs.hash("HEAD");
     var hashToWcDiff = diff.nameStatus(diff.diff(headHash));
@@ -1252,6 +1300,8 @@ var diff = {
     return Object.keys(hashToWcDiff).filter(function(p) { return p in headToHashDiff; });
   },
 
+  // **addedOrModifiedFiles()** returns a list of files that have been
+  // added to or modified in the working copy since the last commit.
   addedOrModifiedFiles: function() {
     var headToc = refs.hash("HEAD") ? objects.commitToc(refs.hash("HEAD")) : {};
     var wc = diff.nameStatus(diff.tocDiff(headToc, index.workingCopyToc()));
