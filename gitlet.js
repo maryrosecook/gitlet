99070f5faa3391d0efa9c5fb93f261b3a3e1ca0a
Mary Rose Cook <maryrosecook@maryrosecook.com>
2015-01-20 12:16:54 -0500

    Add comments for `files` module

--- gitlet.js
+++ gitlet.js
@@ -1689,25 +1689,39 @@ var util = {
   }
 };
 
+// Files module
+// ------------
+
 var files = {
+
+  // **inRepo()** returns true if the current working directory is
+  // inside a repository.
   inRepo: function() {
     return files.gitletPath() !== undefined;
   },
 
+  // **assertInRepo()** throws if the current working directory is not
+  // inside a repository.
   assertInRepo: function() {
     if (!files.inRepo()) {
       throw new Error("not a Gitlet repository");
     }
   },
 
+  // **pathFromRepoRoot()** returns `path` relative to the repo root
   pathFromRepoRoot: function(path) {
     return nodePath.relative(files.workingCopyPath(), nodePath.join(process.cwd(), path));
   },
 
+  // **write()** writes `content` to file at `path`, overwriting
+  // anything that is already there.
   write: function(path, content) {
     files.writeFilesFromTree(util.setIn({}, path.split(nodePath.sep).concat(content)), "/");
   },
 
+  // **writeFilesFromTree()** takes `tree` of files as a nested JS obj
+  // and writes all those files to disk taking `prefix` as the root of
+  // the tree.  `tree` format is: `{ a: { b: { c: "filecontent" }}}`
   writeFilesFromTree: function(tree, prefix) {
     Object.keys(tree).forEach(function(name) {
       var path = nodePath.join(prefix, name);
@@ -1723,6 +1737,8 @@ var files = {
     });
   },
 
+  // **rmEmptyDirs()** recursively remotes all the empty directories
+  // inside `path`.
   rmEmptyDirs: function(path) {
     if (fs.statSync(path).isDirectory()) {
       fs.readdirSync(path).forEach(function(c) { files.rmEmptyDirs(nodePath.join(path, c)); });
@@ -1732,12 +1748,16 @@ var files = {
     }
   },
 
+  // **read()** returns the contents of the file at `path` as a
+  // string.  It returns `undefined` if the file doesn't exist.
   read: function(path) {
     if (fs.existsSync(path)) {
       return fs.readFileSync(path, "utf8");
     }
   },
 
+  // **gitletPath()** returns a string made by concatenating `path` to
+  // the absolute path of the `.gitlet` directory of the repository.
   gitletPath: function(path) {
     function gitletDir(dir) {
       if (fs.existsSync(dir)) {
@@ -1760,10 +1780,14 @@ var files = {
     }
   },
 
+  // **gitletPath()** returns a string made by concatenating `path` to
+  // the absolute path of the root of the repository.
   workingCopyPath: function(path) {
     return nodePath.join(nodePath.join(files.gitletPath(), ".."), path || "");
   },
 
+  // **lsRecursive()** returns an array of all the files found in a
+  // recursive search of `path`.
   lsRecursive: function(path) {
     if (!fs.existsSync(path)) {
       return [];
@@ -1777,12 +1801,21 @@ var files = {
     }
   },
 
+  // **nestFlatTree()** takes `obj`, a mapping of file path strings to
+  // content, and returns a nested JS obj where each key represents a sub
+  // directory.  This is the opposite of `flattenNestedTree()`<br/>
+  // eg `nestFlatTree({ "a/b": "me" }); // => { a: { b: "me" }}`
   nestFlatTree: function(obj) {
     return Object.keys(obj).reduce(function(tree, wholePath) {
       return util.setIn(tree, wholePath.split(nodePath.sep).concat(obj[wholePath]));
     }, {});
   },
 
+  // **flattenNestedTree()** takes `tree`, a nested JS object where
+  // each key represents a sub directory and returns a JS object
+  // mapping file path strings to content.  This is the opposite of
+  // `nestFlatTree()`<br/>
+  // eg `flattenNestedTree({ a: { b: "me" }}); // => { "a/b": "me" }`
   flattenNestedTree: function(tree, obj, prefix) {
     if (obj === undefined) { return files.flattenNestedTree(tree, {}, ""); }
 
