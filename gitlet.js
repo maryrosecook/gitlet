d0b2dd72c5daaeeea8ae5ccac60d8440210baf45
Mary Rose Cook <maryrosecook@maryrosecook.com>
2015-01-27 16:14:40 -0500

    la

--- gitlet.js
+++ gitlet.js
@@ -1746,6 +1746,7 @@ var files = {
   // and writes all those files to disk taking `prefix` as the root of
   // the tree.  `tree` format is: `{ a: { b: { c: "filecontent" }}}`
   writeFilesFromTree: function(tree, prefix) {
+    console.log(tree)
     Object.keys(tree).forEach(function(name) {
       var path = nodePath.join(prefix, name);
       console.log(prefix, name)
